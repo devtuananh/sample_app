24946d201294aae9c7450622686ca2da6536ef8d
devtuananh <tran.tuan.anhb@framgia.com>
2018-07-10 20:46:20 +0700

    Chapter 9 Fix

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module SampleApp
-  class Application < Rails::Application
 
+  class Application < Rails::Application
     config.load_defaults 5.2
   end
 end
