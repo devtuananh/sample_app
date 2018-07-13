0aca3e55462ed397b5a33fc23ca975692bfbb8e5
devtuananh <tran.tuan.anhb@framgia.com>
2018-07-13 09:38:58 +0700

    Chapter 11. Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == Settings.session_controller.session ?
-        remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == Settings.session_controller.session ?
+          remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t ".message"
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = t ".danger"
+      flash.now[:danger] = t ".invalid"
       render :new
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,40 @@
 class User < ApplicationRecord
-  
-  attr_accessor :remember_token
-  before_save {email.downcase!}
-  validates :name, presence: true, length: {maximum: Settings.validates.name.maximum}
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
+  validates :name, presence: true, length:
+    {maximum: Settings.validates.name.maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.validates.email.maximum},
-    format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
+  validates :email, presence: true, length:
+    {maximum: Settings.validates.email.maximum},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.validates.password.minimum}, 
-    allow_nil: true
+  validates :password, presence: true, length:
+    {minimum: Settings.validates.password.minimum}, allow_nil: true
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def remember
+    remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def activate
+    update_attributes activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
   class << self
     def digest string
@@ -22,16 +48,14 @@ class User < ApplicationRecord
     end
   end
 
-  def remember
-    self.remember_token = User.new_token
-    update_attributes remember_digest: User.digest(remember_token)
+  private
+
+  def downcase_email
+    email.downcase!
   end
 
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
-  def forget
-    update_attributes remember_digest: User.digest(remember_token)
+  def create_activation_digest
+    activation_token = User.new_token
+    activation_digest = User.digest activation_token
   end
 end
